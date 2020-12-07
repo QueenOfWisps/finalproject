78bbf4b0ddced7dd26b7f51d529228ba10d419f1
QueenOfWisps <brittainr@my.lanecc.edu>
2020-12-06 17:15:41 -0800

    Initial commit

---
+++ Controllers/QuizController1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Models;
+using FinalProject.Repos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Vquiz()
+        {
+            QuizTools model = new QuizTools();
+            return View(model);
+
+        }
+
+      
+
+        [HttpPost]
+        public IActionResult Vquiz(QuizTools model)
+        {
+            var quizModel = new QuizModel();//new instance of quiz model.
+            if (model.Season == "winter")
+            {
+                quizModel.QuizToolInstance = model;// accessing the data entered from quiztools instancw. 
+                // from the quiz model.
+                quizModel.Score = 5;// change score. replace score with int in own table.
+            }
+
+            model.Score = quizModel.Score;
+            //var repo = new FinalContext();
+            //repo.QuizResults.Add(quizModel);
+            //repo.SaveChanges();
+
+            return View(model);
+        }
+
+        public IActionResult QuizResult() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/QuizModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class QuizModel
+    {
+        public UserModel Q_user { get; set; }//name
+        [Key]
+        public string QuizID { get; set; }
+        public int Score {get; set;}
+
+        public QuizTools QuizToolInstance { get; set; }// this holds all values in the quiz tools model ie 
+                                                       //parasites and seasons .
+
+
+        public List<QuizModel> Seed()
+        //be sure to call in startup <3
+        {
+            //seeded data inside where its own model lives.
+            var rec = new List<QuizModel>();
+
+            rec.Add(new QuizModel() { Score = 10});
+            rec.Add(new QuizModel() { Score = 2});
+            rec.Add(new QuizModel() { Score = 77});
+            rec.Add(new QuizModel() { Score = 99});
+
+            return rec;
+        }
+    }
+
+}
